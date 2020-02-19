464f5a83f677009333b51ebdebe4026e9534af4c
Jennifer Nugent <jmn2158@columbia.edu>
2020-02-19 15:25:47 -0500

    wrote a function, we shall see if it is the correct one

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,24 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let lowBoundary = 0
+	let upperBoundary = array.length-1
+	let midIndex = Math.floor((upperBoundary-lowBoundary) / 2)
+	if (target < array[0] || target>array[array.length-1]) return false
+	while (midIndex > -1 && midIndex < array.length) {
+		if (array[midIndex]===target) return true
+		if (array[midIndex]<target) {
+			upperBoundary = midIndex
+			midIndex = Math.floor((upperBoundary - lowBoundary) / 2)
+
+		}
+		if (array[midIndex>target]) {
+			lowBoundary = midIndex
+			midIndex = Math.floor((upperBoundary - lowBoundary/ 2)+ lowBoundary)
+
+		}
+	}
+	return false
 };
 
 /*
